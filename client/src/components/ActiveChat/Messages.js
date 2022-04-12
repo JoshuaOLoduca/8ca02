fd0f10fb012092d2b9e10ba48830b2a49b33b036
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-04-12 17:33:09 -0400

    add otherUser avatar to latest read message

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box } from '@material-ui/core';
 import { SenderBubble, OtherUserBubble } from '.';
 import moment from 'moment';
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
+  const [latestReadMessageId, setLatestReadMessageId] = useState(0);
+
+  messages.forEach((message) => {
+    if (
+      message.senderId === userId &&
+      message.read &&
+      latestReadMessageId < message.id
+    ) {
+      setLatestReadMessageId(message.id);
+    }
+  });
+  console.log(messages);
 
   return (
     <Box>
       {messages.map((message) => {
         const time = moment(message.createdAt).format('h:mm');
-
         return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
+          <SenderBubble
+            key={message.id}
+            text={message.text}
+            time={time}
+            isLatestRead={latestReadMessageId === message.id}
+            otherUser={otherUser}
+          />
         ) : (
           <OtherUserBubble
             key={message.id}
--- client/src/components/ActiveChat/SenderBubble.js
+++ client/src/components/ActiveChat/SenderBubble.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Box, Typography } from '@material-ui/core';
+import { Avatar, Box, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -25,10 +25,17 @@ const useStyles = makeStyles(() => ({
     background: '#F4F6FA',
     borderRadius: '10px 10px 0 10px',
   },
+  avatar: {
+    height: 20,
+    width: 20,
+    marginTop: 8,
+  },
 }));
 
-const SenderBubble = ({ time, text }) => {
+const SenderBubble = ({ time, text, otherUser, isLatestRead }) => {
   const classes = useStyles();
+
+  console.log(otherUser);
 
   return (
     <Box className={classes.root}>
@@ -36,6 +43,13 @@ const SenderBubble = ({ time, text }) => {
       <Box className={classes.bubble}>
         <Typography className={classes.text}>{text}</Typography>
       </Box>
+      {isLatestRead && (
+        <Avatar
+          alt={otherUser.username}
+          src={otherUser.photoUrl}
+          className={classes.avatar}
+        />
+      )}
     </Box>
   );
 };
