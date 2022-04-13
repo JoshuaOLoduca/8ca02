549d06197c1ad1687b1b6e513377237c443cb24c
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-04-13 13:48:33 -0400

    add draft of database update to support group chats

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,6 +1,8 @@
 const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
+const Group = require("./group");
+const Participant = require("./participant");
 
 // associations
 
@@ -10,8 +12,15 @@ Conversation.belongsTo(User, { as: "user2" });
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
 
+// Group Chat
+User.hasMany(Participant);
+Participant.belongsTo(Group);
+// Message.belongsTo(Group);
+Group.hasMany(Message);
+Group.hasMany(Participant);
+
 module.exports = {
   User,
   Conversation,
-  Message
+  Message,
 };
