513ade3223e2c5ae0892729ef340027c33c09018
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-04-15 15:28:43 -0400

    going for option 1. setup DB for migration

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -1,6 +1,7 @@
 const { Op } = require("sequelize");
 const db = require("../db");
 const Message = require("./message");
+const Participant = require("./participant");
 
 const Conversation = db.define("conversation", {});
 
@@ -9,13 +10,11 @@ const Conversation = db.define("conversation", {});
 Conversation.findConversation = async function (user1Id, user2Id) {
   const conversation = await Conversation.findOne({
     where: {
-      user1Id: {
-        [Op.or]: [user1Id, user2Id]
+      userId: {
+        [Op.or]: [user1Id, user2Id],
       },
-      user2Id: {
-        [Op.or]: [user1Id, user2Id]
-      }
-    }
+    },
+    include: [{ model: Participant }],
   });
 
   // return conversation or null if it doesn't exist
